772d8032645abf35f964782c566297a92247cb8d
中嶋空偉 <sorai.s.nakajima@gmail.com>
2025-10-05 17:11:54 +0900

    ubuntu用に変更

--- setup.sh
+++ setup.sh
@@ -16,11 +16,11 @@ sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 # DockerのGPGキーを追加
 echo "DockerのGPGキーを追加中..."
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Dockerリポジトリを追加
 echo "Dockerリポジトリを追加中..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Dockerをインストール
 echo "Dockerをインストール中..."
