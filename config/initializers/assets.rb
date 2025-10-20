f3e64f3fa080525d8a03321fdd28563f7cdb8e41
中嶋空偉 <sorai.s.nakajima@gmail.com>
2025-10-20 09:38:47 +0900

    asset error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# Add app/javascript to the asset load path for Propshaft
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
