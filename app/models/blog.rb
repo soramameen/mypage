0d587127f0aa35788ae2dbf911294d6823444ce4
中嶋空偉 <sorai.s.nakajima@gmail.com>
2025-10-20 14:43:51 +0900

    記事番号付与

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,12 @@ class Blog < ApplicationRecord
   def content_html
     Kramdown::Document.new(content).to_html.html_safe
   end
+
+  def blog_number
+    Blog.where("created_at <= ?", created_at).count
+  end
+
+  def numbered_title
+    "##{blog_number} #{title}"
+  end
 end
